b63524fb672dfca46ad7be387d26f11a83c6c267
srbhsameer <48203297+srbhsameer@users.noreply.github.com>
2019-05-09 15:43:48 +0530

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Palindrome
+
+# In[ ]:
+
+
+# function which return reverse of a string 
+def reverse(s): 
+    return s[::-1] 
+  
+def isPalindrome(s): 
+    # Calling reverse function 
+    rev = reverse(s) 
+  
+    # Checking if both string are equal or not 
+    if (s == rev): 
+        return True
+    return False
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
